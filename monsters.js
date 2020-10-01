e82a43592db2e23cc0b3a8b503f66b662038bd00
jjlittle9 <65914772+jjlittle9@users.noreply.github.com>
2020-10-01 14:52:29 -0500

    Add files via upload

---
+++ monsters.js
@@ -0,0 +1,17 @@
+var listOfMonsters = [
+  {
+    name: "Animated Knife",
+    hp: 20,
+    dmg: 5
+	},
+  {
+    name: "Air Elemental",
+    hp: 90,
+    dmg: 14
+	},
+  {
+    name: "Adult Black Dragon",
+    hp: 195,
+    dmg: 35
+	}
+]
